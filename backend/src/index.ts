6326bb5a798df0416669289a080f2e434ea3e606
Tushar Karande <tusharkarande1346@gmail.com>
2024-03-14 19:30:51 +0530

    connect database

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,17 +1,23 @@
-import express, { Request, Response } from 'express';
-import cors from 'cors';
-import "dotenv/config"
+import express, { Request, Response } from "express";
+import cors from "cors";
+import "dotenv/config";
+import mongoose from "mongoose";
 
 const app = express();
 const PORT: number = 5000;
 
+mongoose
+  .connect(process.env.MONGODB_URI as string)
+  .then(() => console.log("Connected to database!"))
+  .catch((error) => console.log(error));
+
 app.use(express.json());
 app.use(cors());
 
-app.get('/test', (_req: Request, res: Response) => {
-    res.json({msg: "Hello"})
-})
+app.get("/test", (_req: Request, res: Response) => {
+  res.json({ msg: "Hello" });
+});
 
-app.listen(PORT,() => {
-  console.log(`server running at port:${PORT}`)
-})
+app.listen(PORT, () => {
+  console.log(`server running at port:${PORT}`);
+});
